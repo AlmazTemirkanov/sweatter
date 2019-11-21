caecbeb104f6d1951a7a28a8ba7f227a52374cee
Almaz Temirkanov <atemirkanov89@gmail.com>
2019-11-21 17:55:07 +0600

    add new file

---
+++ src/main/java/com/example/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam
+                    (name="name", required=false, defaultValue="World")
+                    String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(
+            @RequestParam(name="name", required=false, defaultValue="main page") String name, Model model) {
+        model.addAttribute("name", name);
+        return "main";
+    }
+
+}
