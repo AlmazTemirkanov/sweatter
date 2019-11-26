8e89d5a4592b9c1853376e47ed763521e7ee3f89
Almaz Temirkanov <atemirkanov89@gmail.com>
2019-11-26 17:19:09 +0600

    add database and add main page

--- src/main/java/com/example/domain/Message.java
+++ src/main/java/com/example/domain/Message.java
@@ -9,12 +9,46 @@ import javax.persistence.Id;
 public class Message {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+
+//    private String region;
+//    private String district;
+//    private String area;
+//    private String selo;
+//    private String voice;
+//    private String WCDMA;
+//    private String LTE;
+
+
+
+
+
     private Integer id;
-    private String text;
-    private String tag;
 
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String text;
+    private String tag;
+    public Message(){ }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 
     public String getText() {
--- src/main/java/com/example/repos/MessageRepo.java
+++ src/main/java/com/example/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package com.example.repos;
 import com.example.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
