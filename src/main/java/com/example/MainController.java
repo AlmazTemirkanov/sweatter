d8c970a4e0fb4e87e8bac50312edb4a77efe7a3d
Almaz Temirkanov <atemirkanov89@gmail.com>
2019-11-27 15:47:07 +0600

    refractor mainContriller

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -4,14 +4,13 @@ import com.example.domain.Message;
 import com.example.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
